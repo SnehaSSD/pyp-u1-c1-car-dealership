d76c57b2948b1f1820e52d79bda62120d8df2f0a
sneha.shinde19 <sneha.shinde19@gmail.com>
2018-07-21 22:26:45 -0400

    vehicle and customer files

--- dealership/customers.py
+++ dealership/customers.py
@@ -1,11 +1,19 @@
 class Person(object):
-    def __init__(self, first_name, last_name, email):
-        pass
-
+   def __init__(self, first_name, last_name, email):
+      self.first_name = first_name
+      self.last_name = last_name
+      self.email = email
+  
 
 class Customer(Person):
-    pass
+  pass
+    
+  def is_employee(self):
+    return False
 
 
 class Employee(Person):
-    pass
+   DISCOUNT = 10
+    
+  def is_employee(self):
+     return True
